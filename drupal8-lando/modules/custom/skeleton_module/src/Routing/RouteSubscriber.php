1b40629b5bc24903577ec9549f100e6d34489d7e
ChS <32848256+muym-chs@users.noreply.github.com>
2022-01-10 00:01:31 +0100

    drupal8-lando: scaffold drupal generate:routesubscriber

---
+++ drupal8-lando/modules/custom/skeleton_module/src/Routing/RouteSubscriber.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\skeleton_module\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubscriber.
+ *
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+  }
+}
