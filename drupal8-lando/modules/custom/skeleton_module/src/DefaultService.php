ef215cf504820bddd044bbe1561976d750fbc86e
ChS <32848256+muym-chs@users.noreply.github.com>
2022-01-09 23:33:18 +0100

    drupal8-lando: scaffold drupal generate:service

---
+++ drupal8-lando/modules/custom/skeleton_module/src/DefaultService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\skeleton_module;
+
+/**
+ * Class DefaultService.
+ */
+class DefaultService implements DefaultServiceInterface {
+
+  /**
+   * Constructs a new DefaultService object.
+   */
+  public function __construct() {
+
+  }
+
+}
