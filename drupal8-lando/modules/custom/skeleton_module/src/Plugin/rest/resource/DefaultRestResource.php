b41e1eea01092e0b3dd189b4a3846e6b095f7f6f
ChS <32848256+muym-chs@users.noreply.github.com>
2022-01-11 20:42:45 +0100

    drupal8-lando: scaffold drupal generate:plugin:rest:resource

---
+++ drupal8-lando/modules/custom/skeleton_module/src/Plugin/rest/resource/DefaultRestResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\skeleton_module\Plugin\rest\resource;
+
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "default_rest_resource",
+ *   label = @Translation("Default rest resource"),
+ *   uri_paths = {
+ *     "canonical" = "/api/default"
+ *   }
+ * )
+ */
+class DefaultRestResource extends ResourceBase {
+
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->logger = $container->get('logger.factory')->get('skeleton_module');
+    $instance->currentUser = $container->get('current_user');
+    return $instance;
+  }
+
+    /**
+     * Responds to GET requests.
+     *
+     * @param string $payload
+     *
+     * @return \Drupal\rest\ResourceResponse
+     *   The HTTP response object.
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     *   Throws exception expected.
+     */
+    public function get($payload) {
+
+        // You must to implement the logic of your REST Resource here.
+        // Use current user after pass authentication to validate access.
+        if (!$this->currentUser->hasPermission('access content')) {
+            throw new AccessDeniedHttpException();
+        }
+
+        return new ResourceResponse($payload, 200);
+    }
+
+}
