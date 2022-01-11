ca15deeeaa6310e6bc8982ae2d6ba5ac8387bcbf
ChS <32848256+muym-chs@users.noreply.github.com>
2022-01-11 20:48:05 +0100

    drupal8-lando: scaffold drupal generate:plugin:type:yaml

---
+++ drupal8-lando/modules/custom/skeleton_module/src/ExamplePluginYamlManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\skeleton_module;
+
+use Drupal\Component\Plugin\PluginManagerInterface;
+
+/**
+ * Defines an interface for example_plugin_yaml managers.
+ */
+interface ExamplePluginYamlManagerInterface extends PluginManagerInterface {
+
+    // Add getters and other public methods for example_plugin_yaml managers.
+
+}
