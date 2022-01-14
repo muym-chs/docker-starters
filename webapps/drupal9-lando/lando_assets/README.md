e76d71bb27c145ef449477f6838ce8ab07e81b0b
ChS <32848256+muym-chs@users.noreply.github.com>
2022-01-14 23:17:34 +0100

    Add Drupal 9.3.2 vanilla

---
+++ webapps/drupal9-lando/lando_assets/README.md
@@ -0,0 +1,15 @@
+
+CONTENTS OF THIS FILE
+---------------------
+
+ * About
+
+
+About
+------------
+
+DB export:
+ * lando db-export lando_assets/db_dump/drupal8_`date "+%Y-%m-%d%H:%M:%S"`
+
+Automatic DB load:
+ * lando_assets/db_dump/drupal8_initrecoverymount.gz
---
+++ webapps/drupal9-lando/lando_assets/scripts/init_appserver.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ ! -z $LANDO_MOUNT ]; then
+  ##################################
+  # Install app.                   #
+  ##################################
+  composer install;
+  ##################################
+  # Load app settings.             #
+  ##################################
+  if [ ! -e "/app/sites/default/settings.php" ]
+    then
+      ./lando_assets/scripts/prepare_appserver_install.sh
+  fi
+  cp ./lando_assets/appserver/settings.php /app/sites/default/
+  ##################################
+  # Drop and recover last dump.    #
+  ##################################
+  drush sql-drop -y;
+  zcat lando_assets/db_dump/drupal8_initrecoverymount.sql.gz | drush sql-cli;
+  drush cr;
+fi
