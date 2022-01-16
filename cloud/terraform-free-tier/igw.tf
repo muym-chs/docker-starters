4c01160041b56045874b4a92c13d334e69f967ad
ChS <32848256+muym-chs@users.noreply.github.com>
2022-01-16 22:39:16 +0100

    Add tmux 23 omt base

--- cloud/terraform-free-tier/igw.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.new_vpc.id
-  tags = {
-    Name = var.internet_gateway_name
-  }
-}
--- cloud/terraform-free-tier/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "new_vpc" {
-  cidr_block           = var.vpc_cidr_block
-  enable_dns_hostnames = "true"
-  enable_dns_support   = "true"
-  tags = {
-    Name = var.vpc_name
-  }
-}
